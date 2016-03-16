14df4dc473b5777929e82f67a325fb2d9176787b
Martin Heidegger <martin.heidegger@gmail.com>
2016-03-16 22:31:17 +0900

    hyperquest first steps

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-module.exports = function (slug, branch, callback) {
+var hq = require('hyperquest')
+var toArray = require('stream-to-array')
+function ls (slug, branch, callback) {
   setImmediate(function () {
     callback(null, [])
   })
 }
+ls.request = function (url, options, callback) {
+  if (!options.headers) {
+    options.headers = {}
+  }
+  options.headers['User-Agent'] = 'SVN/1.7.20 neon/0.29.6'
+  options.headers['Accept-Encoding'] = 'gzip'
+  options.headers['Content-Type'] = 'text/xml'
+  options.headers['Host'] = 'github.com'
+  options.headers['Connection'] = 'TE'
+  options.headers['TE'] = 'trailers'
+  options.headers['DAV'] = [
+    'http://subversion.tigris.org/xmlns/dav/svn/depth',
+    'http://subversion.tigris.org/xmlns/dav/svn/mergeinfo',
+    'http://subversion.tigris.org/xmlns/dav/svn/log-revprops'
+  ].join(';')
+  var info
+  var stream = hq(url, options, function (err, _info) {
+    info = _info
+    if (err) {
+      return callback(err, info)
+    }
+  })
+  toArray(stream, function (err, parts) {
+    if (err) {
+      return callback(err, info)
+    }
+    callback(err, info, Buffer.concat(parts.map(function (part) {
+      if (part instanceof Buffer) return part
+      return new Buffer(part)
+    })))
+  })
+}
+module.exports = ls
--- test/index.js
+++ test/index.js
@@ -1,12 +1,21 @@
 var test = require('tap').test
 var ls = require('..')
+var REPO = 'martinheidegger/github-ls'
 
 test('list files of master branch', function (t) {
-  ls('martinheidegger/github-ls', 'master', function (err, list) {
+  ls(REPO, 'master', function (err, list) {
     t.equal(err, null)
     if (!Array.isArray(list)) {
       t.fail('List should be an array')
     }
+    t.end()
+  })
+})
+test('handshake request', function (t) {
+  ls.request('https://github.com/' + REPO + '/master', {}, function (err, res, body) {
+    t.equal(err, null)
+    t.equal(res.statusCode, 200)
+    t.equal(body.toString(), '')
     t.end()
   })
 })
