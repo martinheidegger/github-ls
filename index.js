3be1dfb7eff6d715c5c742d598121ed83957e5e2
Martin Heidegger <martin.heidegger@gmail.com>
2016-03-17 02:13:26 +0900

    Further tests with the svn format and cheerio

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var hq = require('hyperquest')
 var toArray = require('stream-to-array')
+var cheerio = require('cheerio')
 function ls (slug, branch, callback) {
   setImmediate(function () {
     callback(null, [])
@@ -11,7 +12,7 @@ ls.request = function (url, options, callback) {
   }
   options.headers['User-Agent'] = 'SVN/1.7.20 neon/0.29.6'
   options.headers['Accept-Encoding'] = 'gzip'
-  options.headers['Content-Type'] = 'text/xml'
+  options.headers['Content-Type'] = 'text/xml; charset=utf-8'
   options.headers['Host'] = 'github.com'
   options.headers['Connection'] = 'TE'
   options.headers['TE'] = 'trailers'
@@ -20,6 +21,12 @@ ls.request = function (url, options, callback) {
     'http://subversion.tigris.org/xmlns/dav/svn/mergeinfo',
     'http://subversion.tigris.org/xmlns/dav/svn/log-revprops'
   ].join(';')
+
+  var body
+  if (options.body) {
+    body = options.body
+    delete options.body
+  }
   var info
   var stream = hq(url, options, function (err, _info) {
     info = _info
@@ -27,14 +34,19 @@ ls.request = function (url, options, callback) {
       return callback(err, info)
     }
   })
+  if (body) {
+    stream.write(body)
+    stream.end()
+  }
   toArray(stream, function (err, parts) {
     if (err) {
       return callback(err, info)
     }
-    callback(err, info, Buffer.concat(parts.map(function (part) {
+    var buffer = Buffer.concat(parts.map(function (part) {
       if (part instanceof Buffer) return part
       return new Buffer(part)
-    })))
+    }))
+    callback(err, info, cheerio.load(buffer, {xmlMode: true}), buffer)
   })
 }
 module.exports = ls
--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 var test = require('tap').test
 var ls = require('..')
 var REPO = 'martinheidegger/github-ls'
+var GITHUB = 'https://github.com'
+var GITHUB_REPO = GITHUB + '/' + REPO
 
 test('list files of master branch', function (t) {
   ls(REPO, 'master', function (err, list) {
@@ -12,10 +14,54 @@ test('list files of master branch', function (t) {
   })
 })
 test('handshake request', function (t) {
-  ls.request('https://github.com/' + REPO + '/master', {}, function (err, res, body) {
+  ls.request(GITHUB_REPO, {
+    method: 'OPTIONS',
+    body: '<?xml version="1.0" encoding="utf-8"?><D:options xmlns:D="DAV:"><D:activity-collection-set/></D:options>'
+  }, function (err, res, $, body) {
     t.equal(err, null)
     t.equal(res.statusCode, 200)
-    t.equal(body.toString(), '')
-    t.end()
+    var nextHref = $.xml($('D\\:href')[0].children)
+    t.equal(nextHref, '/' + REPO + '/!svn/act/')
+    ls.request(GITHUB_REPO, {
+      method: 'PROPFIND',
+      headers: {
+        Depth: '0'
+      },
+      body: '<?xml version="1.0" encoding="utf-8"?><propfind xmlns="DAV:"><prop><version-controlled-configuration xmlns="DAV:"/><resourcetype xmlns="DAV:"/><baseline-relative-path xmlns="http://subversion.tigris.org/xmlns/dav/"/><repository-uuid xmlns="http://subversion.tigris.org/xmlns/dav/"/></prop></propfind>'
+    }, function (err, res, $, body) {
+      t.equal(err, null)
+      t.equal(res.statusCode, 207)
+      var nextHref = $.xml($('lp1\\:version-controlled-configuration D\\:href')[0].children)
+      t.equal(nextHref, '/' + REPO + '/!svn/vcc/default')
+      ls.request(GITHUB_REPO + '/!svn/vcc/default', {
+        method: 'PROPFIND',
+        headers: {
+          Depth: '0'
+        },
+        body: '<?xml version="1.0" encoding="utf-8"?><propfind xmlns="DAV:"><prop><checked-in xmlns="DAV:"/></prop></propfind>'
+      }, function (err, res, $, body) {
+        t.equal(err, null)
+        t.equal(res.statusCode, 207)
+        var nextHref = $.xml($('lp1\\:checked-in D\\:href')[0].children)
+        ls.request(GITHUB + nextHref, {
+          method: 'PROPFIND',
+          headers: {
+            Depth: '1'
+          },
+          body: '<?xml version="1.0" encoding="utf-8"?><propfind xmlns="DAV:"><prop><creator-displayname xmlns="DAV:"/><creationdate xmlns="DAV:"/><version-name xmlns="DAV:"/><deadprop-count xmlns="http://subversion.tigris.org/xmlns/dav/"/><getcontentlength xmlns="DAV:"/><resourcetype xmlns="DAV:"/></prop></propfind>'
+        }, function (err, res, $, body) {
+          t.equal(err, null)
+          t.equal(res.statusCode, 207)
+          var links = $('D\\:href').toArray().map(function (node) {
+            return $.xml(node.children)
+          })
+          links = links.map(function (link) {
+            return link.substr(links[0].length)
+          })
+          t.deepEqual(links, ['', 'trunk/', 'branches/'])
+          t.end()
+        })
+      })
+    })
   })
 })
